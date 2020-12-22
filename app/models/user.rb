4d190f6a2db393b30de14c66d3bf7fa0a3eb7184
adrien nhem <adrien.nhem@gmail.com>
2020-12-22 10:31:40 +0000

    ready for submission

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  
+  has_many :sessions
+  has_many :tasks
+
+  validates :username, length: { minimum: 3, maximum: 64 }, presence: true
+  validates :password, length: { minimum: 8, maximum: 64 }, presence: true
+  validates_uniqueness_of :username
 end
